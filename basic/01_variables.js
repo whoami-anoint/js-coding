39cb804545c77bf0aedf21801475653e22f989ec
whoami-anoint <abhishekkfl@gmail.com>
2023-06-07 01:27:38 +0530

    variables

---
+++ basic/01_variables.js
@@ -0,0 +1,15 @@
+const accountID = 21129
+let eduMail = "21bsdc129@gcu.edu.in"
+var passwd = "12345"
+accountCity = "Bangalore"
+
+/* Don't use var 
+https://medium.com/@codingsam/awesome-javascript-no-more-var-working-title-999428999994
+*/
+
+// accountID = 2111 //not allowed
+eduMail = "22bsdc129@gcu.edu.in"
+passwd = "678910"
+console.log(accountID,eduMail,passwd,accountCity)
+
+console.table([accountID,eduMail,passwd,accountCity])
