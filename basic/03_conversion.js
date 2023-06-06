757d17e40dfe26f882526446f5f2fd1a64a18a06
whoami-anoint <abhishekkfl@gmail.com>
2023-06-06 21:35:28 +0530

    conversion of datatypes

---
+++ basic/03_conversion.js
@@ -0,0 +1,25 @@
+let score = "32a"
+
+// console.log(typeof score)
+
+let valueNum = Number(score)
+// console.log(valueNum);
+// console.log(typeof valueNum)
+// console.log(typeof NaN);
+
+// "32" => 32
+// "32ab" =? NaN
+// true ==> 1; false = 0 
+
+let isLogedIn = 1
+let booleanisLogedIn = Boolean(isLogedIn)
+// console.log(booleanisLogedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "anoint" => true
+
+let pc = 12
+// console.log("value:",pc)
+tc = String(pc)
+//  console.log(typeof tc);
