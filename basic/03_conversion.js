fc8a823819751135c07e205a0e00cbf12c5a8b2c
whoami-anoint <abhishekkfl@gmail.com>
2023-06-06 23:22:37 +0530

    conversion

--- basic/03_conversion.js
+++ basic/03_conversion.js
@@ -23,3 +23,59 @@ let pc = 12
 // console.log("value:",pc)
 tc = String(pc)
 //  console.log(typeof tc);
+
+
+// ******************** Operations *******************
+
+let value = 3 
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "Hello"
+let str2 = " Abhishek"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1"+ 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log(true);
+// console.log(+true);
+// console.log(+"");
+
+// let num1, num2, num3
+// num1 = num2 = num3 = 2 + 2 
+// console.log(num1);
+
+let gameCounter = 100
+++gameCounter
+// console.log(gameCounter);
+
+let x = 4;
+let y = x++;
+console.table([x,y]);
+
+
+let a = 6
+let b = ++a
+console.table([a,b])
+
+/* If used postfix, with operator after operand (for example, x++),
+the increment operator increments and returns the value before incrementing.
+
+If used prefix, with operator before operand (for example, ++a), 
+the increment operator increments and returns the value after incrementing.
+*/
+
+
