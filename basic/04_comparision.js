6a3d756a020850e826eae2c928d07f096e525171
whoami-anoint <abhishekkfl@gmail.com>
2023-06-07 00:51:53 +0530

    comparision

---
+++ basic/04_comparision.js
@@ -0,0 +1,18 @@
+// console.log("02"> 1);
+// console.log("02">1);
+
+console.log(null > 0 );
+console.log(null == 0);
+console.log(null >= 0);
+
+/* comparision and equality check are different
+comparision => < > 
+equality check => ==
+
+In comparision, null converted to 0 
+*/
+console.log("/****\\")
+
+console.log(undefined < 0 );
+console.log(undefined == 0);
+console.log(undefined < 0);
