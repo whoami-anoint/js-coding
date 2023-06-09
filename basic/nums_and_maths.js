81d8f8b469ca6c4f3627c24ee3fd76494903182d
whoami-anoint <abhishekkfl@gmail.com>
2023-06-09 17:35:11 +0530

    Nums and Maths

---
+++ basic/nums_and_maths.js
@@ -0,0 +1,25 @@
+const score = 400
+// console.log(score);
+
+const balance = new Number(24)
+// console.log(balance);
+// console.log(balance.toString().length); Number to string and count length
+// console.log(balance.toFixed(2)); // make fixed the number 24 => 24.00
+
+const otherNumber = 123.8966
+// console.log(otherNumber.toPrecision(2));   // exponential or fixed-point notation with a specified number of digits
+// roundoff then ouput will be : 124
+
+const hundreds = 1000000
+// console.log(hundreds.toLocaleString('en-IN')); Indian standard number system
+
+// ++++++++++++++++ Maths +++++++++++++++++++
+
+// Math functions
+
+// console.log(Math.PI); // Value of PI
+// console.log(Math.abs(-4)); // Negative to positive
+// console.log(Math.pow(5,2)); // 5 power 2 => 25 
+// console.log(Math.round(4.4)); // Roundoff and output => // Only above 4.5 it will 5, till 4.4 it will be 4
+// console.log(Math.ceil(4.2)); // If more than top value (4) then output => 5
+// console.log(Math.floor(4.9)); // It take lowest value; Output => 4 even in 4.9
