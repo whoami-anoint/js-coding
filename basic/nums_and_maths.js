6668200c45e1edecac1b97e9ad9ecfbd9c13e92b
whoami-anoint <abhishekkfl@gmail.com>
2023-06-10 20:25:59 +0530

    Num and math

--- basic/nums_and_maths.js
+++ basic/nums_and_maths.js
@@ -20,6 +20,9 @@ const hundreds = 1000000
 // console.log(Math.PI); // Value of PI
 // console.log(Math.abs(-4)); // Negative to positive
 // console.log(Math.pow(5,2)); // 5 power 2 => 25 
+// console.log(Math.sqrt(25)); // find square root of number
 // console.log(Math.round(4.4)); // Roundoff and output => // Only above 4.5 it will 5, till 4.4 it will be 4
 // console.log(Math.ceil(4.2)); // If more than top value (4) then output => 5
 // console.log(Math.floor(4.9)); // It take lowest value; Output => 4 even in 4.9
+
+console.log(Math.random());
