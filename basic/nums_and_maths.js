15928c1e96445a550b133cd8f1c1a9d67049fc49
whoami-anoint <abhishekkfl@gmail.com>
2023-06-11 02:14:09 +0530

    num

--- basic/nums_and_maths.js
+++ basic/nums_and_maths.js
@@ -25,4 +25,5 @@ const hundreds = 1000000
 // console.log(Math.ceil(4.2)); // If more than top value (4) then output => 5
 // console.log(Math.floor(4.9)); // It take lowest value; Output => 4 even in 4.9
 
-console.log(Math.random());
+// console.log(Math.random());
+// console.log(Math.min(4,5,6,7,2));
