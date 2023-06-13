4ea537be1dc45cdcfd2dfed1e0d1672d39e758fa
whoami-anoint <abhishekkfl@gmail.com>
2023-06-13 23:24:51 +0530

    flat & infinity in arrat

--- basic02/02_array.js
+++ basic02/02_array.js
@@ -16,7 +16,7 @@ const all_new_heros = [...marval_heros,...dc_heros]
 // ... is most preferable way to concat two arrays
 
 // const another_array = [1,2,3,[4,5,6],7,[6,7],[4,5]]
-// const real_another_array = another_array.flat(Infinity)
+const real_another_array = another_array.flat(Infinity)
 // console.log(real_another_array);
 
 // console.log(Array.isArray(marval_heros));
