fadd8b499efe25bca6986205f0d9807df03b70d2
whoami-anoint <abhishekkfl@gmail.com>
2023-06-17 23:04:30 +0530

    objects

---
+++ basic02/04_objects.js
@@ -0,0 +1,36 @@
+// const tinderUser = new Object()
+const tinderUser = {}
+
+tinderUser.id = "21BSDC129"
+tinderUser.name = "Abishek"
+tinderUser.isLoggedIn = false
+
+// console.log(tinderUser);
+
+const regularUser = {
+    email: "some@gmail.com",
+    fullName: {
+        userFullname: {
+            firstname: "Abishek",
+            lastname : "Kafle"
+        }
+    }
+}
+
+console.log(regularUser.fullName .userFullname.firstname);
+
+
+const obj1 = {1:"a",2:"b"}
+const obj2 = {3:"a",4:"b"}
+
+// const obj3 = Object.assign({},obj1,obj2)
+
+const obj3 = {...obj1,...obj2} // mostly used 
+console.log(obj3);
+
+const users = [
+    {
+        id: 1,
+        
+    }
+]
