91d67b54d06c76bcd023ef813a5534c3cf64eff5
whoami-anoint <abhishekkfl@gmail.com>
2023-06-22 23:11:20 +0530

    global and block scope

---
+++ basic03/03_scopes.js
@@ -0,0 +1,26 @@
+/*
+if (true){
+    let a = 10
+    const b = 20
+    var c = 30
+}
+
+
+// console.log(a);
+// console.log(b);
+console.log(c);
+*/  
+
+// console.log(myVariable); // Output: undefined
+// var myVariable = 'Hello!';
+
+/*
+let a = 300
+if (true){
+    let a = 10
+    const b = 20
+    console.log("Inner:",a); // block scope
+}
+console.log(a); // global scope
+*/
+
