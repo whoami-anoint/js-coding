d8d070848a1e0f2f51c10e02fb171bcb8501e0b7
whoami-anoint <abhishekkfl@gmail.com>
2023-06-23 01:01:54 +0530

    arrow function, implicit, this

---
+++ basic03/04_arrow.js
@@ -0,0 +1,34 @@
+const user = {
+    username: "anoint",
+    price: 999,
+    isLogged : true,
+
+//     welcomeMessage: function(){
+//         console.log(`${this.username}, welcome to this website`);
+//         console.log(this); // prints current context
+
+//     }
+}
+
+
+// user.welcomeMessage()
+
+// user.username = "Abhi"
+// user.welcomeMessage() // Abhi, welcome to this website because of the value changes
+
+
+// const greet = () => {
+//     console.log("Good night")
+// }
+// greet()
+
+/*
+const addTwo = (num1,num2) => num1 + num2 // implicit return
+
+console.log(addTwo(5,7));
+*/ 
+
+/*
+const sleepTime = () => console.log("goodnight dada");
+sleepTime()
+*/
