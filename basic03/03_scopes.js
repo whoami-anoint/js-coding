2a1167c00481155369dbeafccf6c580c5238ea03
whoami-anoint <abhishekkfl@gmail.com>
2023-06-23 00:18:06 +0530

    global scope, block scope, hoisting

--- basic03/03_scopes.js
+++ basic03/03_scopes.js
@@ -24,3 +24,40 @@ if (true){
 console.log(a); // global scope
 */
 
+function one(){
+    const username = "whoami-anoint"
+
+    function two(){
+        const website = "github"
+        console.log(username);  
+    }
+    // console.log(website);
+
+    two()
+}
+// one()
+
+if(true){
+    const username = "anoint"
+    if (username === "anoint"){
+        const website = "github"
+        // console.log(username,website)
+    }
+    // console.log(website); // out of scope
+}
+// console.log(username); // out of scope
+
+// *******************++++++++++ Interesting ++++++++++*******************
+
+console.log(addOne(5)) // it works here
+
+function addOne(num){
+    return num + 1
+}
+
+// console.log(addTwo(5)); // Error comes because it doesn't works in hoisting
+
+const addTwo = function(num){ // hoisting
+    return num + 2
+}
+
