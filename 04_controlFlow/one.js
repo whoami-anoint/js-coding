31fe71f35da3a2b19a558a98e3c0e91e62328906
whoami-anoint <abhishekkfl@gmail.com>
2023-06-26 22:58:31 +0530

    short hand notation

---
+++ 04_controlFlow/one.js
@@ -0,0 +1,32 @@
+// if
+// const isUserLoggedIn = true
+// const temperature = 28
+
+// if(temperature < 50){
+//      console.log("Less than 50");
+// }else{
+// console.log("Temperature is greater than 50");
+// }
+
+// if (2 === "2"){
+//     console.log("hello");
+// }
+
+// <, > , <=, >=, ==, != , ===, !==
+
+// if (2!=3){
+//     console.log("hello1");
+// }
+
+// const score = 200
+
+// if (score > 100){
+//     let power = "fly"
+//     console.log(`User Power: ${power}`);
+// }
+
+// short hand notation
+
+// const balance = 1000
+
+// if (balance > 500) console.log("test"),console.log("test2");
