904f41939064ca62e6c3c01de5ade4700070b902
whoami-anoint <abhishekkfl@gmail.com>
2023-06-27 23:57:07 +0530

    Terniary operator

--- 04_controlFlow/truthy.js
+++ 04_controlFlow/truthy.js
@@ -16,8 +16,33 @@ console.log("Don't have user email");
 //     console.log("Array is empty");
 // }
 
-const emptyObj = {}
+// const emptyObj = {}
 
-if (Object.keys(emptyObj.length===0)){
-    console.log("Object is empty");
-}
+// if (Object.keys(emptyObj.length===0)){
+//     console.log("Object is empty");
+// }
+
+// const emptyObj = {}
+// if (Object.keys(emptyObj).length ===0){
+//     console.log("object is empty");
+// }
+
+// Nullish coalescing operator (??):null undefined
+
+let val1;
+val1 = 5 ?? 10
+// val1 = null ?? 10 //output: 10
+
+// val1 = undefined ?? 15  // output : 15
+
+val1 = null ?? 10 ?? 15 // value which comes first; output: 10
+
+console.log(val1);
+
+
+// Terniary operator
+
+// condition ? true : false
+
+const iceCreamPrice = 100
+iceCreamPrice <= 80 ? console.log("less than 80"): console.log("more than 80");
