f167b8e4b1d4634fc4a2f211926d00b246328d69
whoami-anoint <abhishekkfl@gmail.com>
2023-06-27 20:34:45 +0530

    update

--- 04_controlFlow/one.js
+++ 04_controlFlow/one.js
@@ -27,6 +27,16 @@
 
 // short hand notation
 
-// const balance = 1000
+const balance = 1000
 
-// if (balance > 500) console.log("test"),console.log("test2");
+// if (balance > 500) console.log("test"),console.log("test2"); // immature code
+
+if (balance < 500){
+    console.log("less than 500");
+}else if(balance < 750){
+    console.log("less than 750");
+}else if(balance < 900){
+    console.log("less than 900");
+}else{
+    console.log("less than 1200");
+}
