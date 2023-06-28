9da9b2a6073adbea3fed3d2a241ee2c2ed73ddaa
whoami-anoint <abhishekkfl@gmail.com>
2023-06-28 22:41:45 +0530

    for loop

--- 05_iteration/one.js
+++ 05_iteration/one.js
@@ -1,9 +1,9 @@
 // for 
 
-for (let i = 0; i < 10; ++i) {
-    const element = i;  
-    console.log(element);
-}
+// for (let i = 0; i < 10; i++) {
+//     const element = i;  
+//     console.log(element);
+// }
 
 // for (let n = 10; n <=100;n++){
 //     console.log(n);
