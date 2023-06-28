dd7ffe04282ca3e666b100f8e7d703da11f07b24
whoami-anoint <abhishekkfl@gmail.com>
2023-06-28 23:21:28 +0530

    update

--- 05_iteration/one.js
+++ 05_iteration/one.js
@@ -1,4 +1,4 @@
-// for 
+// for loop
 
 // for (let i = 0; i < 10; i++) {
 //     const element = i;  
@@ -8,3 +8,11 @@
 // for (let n = 10; n <=100;n++){
 //     console.log(n);
 // }
+
+for (let i = 0; i <= 10; i++) {
+    const element = i; 
+    if (element == 5){
+        console.log("7 is the best number");
+    } 
+    console.log(element);
+}
