ad4389ccc7964864328b49ed9240c2522accf78a
whoami-anoint <abhishekkfl@gmail.com>
2023-06-29 22:51:06 +0530

    array loop

--- 05_iteration/one.js
+++ 05_iteration/one.js
@@ -9,10 +9,29 @@
 //     console.log(n);
 // }
 
-for (let i = 0; i <= 10; i++) {
-    const element = i; 
-    if (element == 5){
-        console.log("7 is the best number");
-    } 
+// for (let i = 0; i <= 10; i++) {
+//     const element = i; 
+//     if (element == 5){
+//         console.log("7 is the best number");
+//     } 
+//     console.log(element);
+// }
+
+for (let i=1;i<=10;i++){
+   // console.log(`Outer loop value: ${i}`);
+    for (let j =1 ; j <=10;j++){
+        // console.log(`Inner loop value: ${j} and inner loop ${i}`);
+        // console.log(i + '*'+ j+ '='+i*j);
+        // console.log(typeof(i , '*', j, '=',i*j));
+
+    }
+}
+let myArray = ["flash","batman","superman"]
+// console.log(myArray.length)
+for (let index = 0; index < myArray.length; index++) {
+    const element = myArray[index];
     console.log(element);
 }
+
+
+// we can concat using , and + but the typeof of ',' makes number and '+' makes string
