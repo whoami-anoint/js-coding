86bd6b38145a5768c7bebfc61699a4b49cdf1bef
whoami-anoint <abhishekkfl@gmail.com>
2023-06-29 23:49:39 +0530

    break in loop

--- 05_iteration/one.js
+++ 05_iteration/one.js
@@ -26,12 +26,23 @@ for (let i=1;i<=10;i++){
 
     }
 }
-let myArray = ["flash","batman","superman"]
-// console.log(myArray.length)
-for (let index = 0; index < myArray.length; index++) {
-    const element = myArray[index];
-    console.log(element);
-}
-
-
 // we can concat using , and + but the typeof of ',' makes number and '+' makes string
+
+// let myArray = ["flash","batman","superman"]
+// // console.log(myArray.length)
+// for (let index = 0; index < myArray.length; index++) {
+//     const element = myArray[index];
+//     console.log(element);
+// }
+
+
+// break and continue
+
+for (let i = 1; i <= 20;i++){
+    if (i ==5){
+        console.log(`Detected 5`);
+        break
+    }
+    
+    console.log(`Value of i is ${i}`);
+}
