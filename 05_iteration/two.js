330a48d04e9cdb30e2ceff21bf48e7233caecab0
whoami-anoint <abhishekkfl@gmail.com>
2023-06-30 00:22:07 +0530

    while loop in array

--- 05_iteration/two.js
+++ 05_iteration/two.js
@@ -1,5 +1,14 @@
-let index = 0;
-while (index <=10) {
-    console.log(`Value of index is ${index}`);
-    index = index + 2
-}
+// let index = 0;
+// while (index <=10) {
+//     console.log(`Value of index is ${index}`);
+//     index = index + 2
+// }
+
+
+// let myArray = ['flash','batman','superman']
+
+// let arr = 0
+// while (arr < myArray.length){
+//     console.log(`Value of array is ${myArray[arr]}`);
+//     arr++
+// }
