fa6cb1d0f4c8ae4860db305a899a6d16783537c2
whoami-anoint <abhishekkfl@gmail.com>
2023-06-29 23:52:31 +0530

    for loop

--- 05_iteration/one.js
+++ 05_iteration/one.js
@@ -38,11 +38,24 @@ for (let i=1;i<=10;i++){
 
 // break and continue
 
+// break
+
+// stops till detected
+
+// for (let i = 1; i <= 20;i++){
+//     if (i ==5){
+//         console.log(`Detected 5`);
+//         break
+//     }    
+//     console.log(`Value of i is ${i}`);
+// }
+
+// continue
+// skip and continue
 for (let i = 1; i <= 20;i++){
     if (i ==5){
         console.log(`Detected 5`);
-        break
-    }
-    
+        continue
+    }    
     console.log(`Value of i is ${i}`);
 }
