ac8751b66a046cfd9e79f95f9daea13aeb158911
whoami-anoint <abhishekkfl@gmail.com>
2023-07-01 22:11:59 +0530

    loop in strings

--- 05_iteration/three.js
+++ 05_iteration/three.js
@@ -7,3 +7,8 @@ const arr = [1,2,3,4,5]
 for (const num of arr) {
     // console.log(num);
 }
+
+const greetings = "Hello world!"
+for (const greet of greetings){
+    console.log(`Each char is ${greet}`);
+}
