cb8d40dfdad6bf36aacc28e5cacbe492953391d0
whoami-anoint <abhishekkfl@gmail.com>
2023-07-02 14:49:05 +0530

    for each

--- 05_iteration/five.js
+++ 05_iteration/five.js
@@ -17,3 +17,25 @@ const coding = ["js","ruby","java","python","golang"]
 
 // coding.forEach(printMe)
 
+// coding.forEach((item,index,arr)=>{
+//     console.log(item,index,arr);
+// })
+
+const myCoding = [
+    {
+        language:"javascript",
+        languageFileName: "js"
+    },
+    {
+        language: "golang",
+        languageFileName: "go"
+    },
+    {
+        language: "python",
+        languageFileName: "py"
+    }
+]
+
+myCoding.forEach((item)=>{
+    console.log(item.language);
+})
