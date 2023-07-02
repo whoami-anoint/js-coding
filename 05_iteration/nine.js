e62edc5881143363cf1661d6f7313c6e95224ee2
whoami-anoint <abhishekkfl@gmail.com>
2023-07-02 23:16:41 +0530

    reduce in javascript

--- 05_iteration/nine.js
+++ 05_iteration/nine.js
@@ -1,8 +1,25 @@
 const myNums = [1,2,3]
 
-const myTotal = myNums.reduce(function(acc,currval){
-    console.log(`acc: ${acc} and currval: ${currval}`);
-    return acc + currval
-},0)
+// Method 1: 
+// const myTotal = myNums.reduce(function(acc,currval){
+//     console.log(`acc: ${acc} and currval: ${currval}`);
+//     return acc + currval
+// },0)
+// console.log(myTotal);
 
-console.log(myTotal);
+
+// Method 2:
+// const myTotal = myNums.reduce( (acc,currval) => acc+currval,0)
+
+// console.log(myTotal);
+
+/*
+const pickupLines = [
+    "Are you an array1?",
+    "Because I want to reduce the distance between us."
+  ];
+  
+  const result = pickupLines.reduce((acc, line) => acc + " " + line);
+  console.log(result);
+*/
+
