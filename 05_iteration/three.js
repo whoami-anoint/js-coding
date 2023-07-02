1b4a5321300c7617f866b3cdee2f563511899bc3
whoami-anoint <abhishekkfl@gmail.com>
2023-07-02 13:38:53 +0530

    loop key and value of map

--- 05_iteration/three.js
+++ 05_iteration/three.js
@@ -8,7 +8,24 @@ for (const num of arr) {
     // console.log(num);
 }
 
-const greetings = "Hello world!"
-for (const greet of greetings){
-    console.log(`Each char is ${greet}`);
+// const greetings = "Hello world!"
+// for (const greet of greetings){
+//     console.log(`Each char is ${greet}`);
+// }
+
+// Maps
+
+const map = new Map()
+map.set('IN',"India")
+map.set('USA',"United States")
+
+// console.log(map);
+
+// for (const key of map){
+//     console.log(key);
+// }
+
+for (const [key,value] of map){
+    console.log(key,':',value);
 }
+
