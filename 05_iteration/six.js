91f62494f190584fa42443eda8928dae32996884
whoami-anoint <abhishekkfl@gmail.com>
2023-07-02 17:25:47 +0530

    filter

---
+++ 05_iteration/six.js
@@ -0,0 +1,22 @@
+// const coding = ["js","ruby","java","python","golang"]
+
+// const values = coding.forEach((item)=>{
+//     console.log(item)
+//     return item
+// })
+
+// console.log(values);
+
+
+// const myNums = [1,2,3,4,5,6,7,8,9,10]
+
+// const newNum = myNums.filter((num) => num>4 )
+// console.log(newNum);
+
+
+const myNums = [1,2,3,4,5,6,7,8,9,10]
+
+const newNum = myNums.filter((num) => {
+    return num>4 
+})
+console.log(newNum);
