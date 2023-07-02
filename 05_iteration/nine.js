b25a8d6632014ae2778956478bb53b13708a85ab
whoami-anoint <abhishekkfl@gmail.com>
2023-07-02 23:29:54 +0530

    reduce in javascript

--- 05_iteration/nine.js
+++ 05_iteration/nine.js
@@ -23,3 +23,29 @@ const pickupLines = [
   console.log(result);
 */
 
+const shoppingCart = [
+    {
+        itemName: "js course",
+        price: 999
+    },
+    {
+        itemName: "python course",
+        price: 1999
+
+    },
+    {
+        itemName: "golang course",
+        price : 2999
+    },
+    {
+        itemName: "java course",
+        price: 3999
+    },
+    {
+        itemName: "hacking course",
+        price: 4999
+    }
+]
+
+const itemPrice = shoppingCart.reduce((acc,item)=>acc+item.price,0)
+console.log(itemPrice);
