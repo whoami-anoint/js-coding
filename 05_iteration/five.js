43eb65cf55bf22b82761c43727f8db627271ca70
whoami-anoint <abhishekkfl@gmail.com>
2023-07-02 14:41:18 +0530

    for each loop

---
+++ 05_iteration/five.js
@@ -0,0 +1,19 @@
+const coding = ["js","ruby","java","python","golang"]
+
+// Method 1 
+// coding.forEach(function(items){
+//     console.log(items);
+// } )
+
+// Method 2
+// coding.forEach((item)=>{
+//     console.log(item);
+// }) 
+
+// Method 3 
+// function printMe(items){
+//     console.log(items)
+// }
+
+// coding.forEach(printMe)
+
